ea8ea779b2429461e2b6372a73225c0726351ff8
Shayan1503 <91187603+Shayan1503@users.noreply.github.com>
2021-09-23 01:04:01 +0530

    Minor Changes
    Removed junk

--- GCD.java
+++ GCD.java
@@ -1,7 +1,7 @@
 /*
  * @author : Shayan Dasgupta
  * 
- * Aim : Finds the GCD of two numbers using the Euclidean Algorithm.
+ * Finds the GCD of two numbers using the Euclidean Algorithm.
  */
 
 import java.io.*;
@@ -15,7 +15,8 @@ class GCD
         System.out.println("Enter the two numbers whose GCD you want to find :");
         int a =Integer.parseInt(br.readLine());
         int b =Integer.parseInt(br.readLine());
-
+        
+        // Program doesn't work when b<a
         if(b<a)
         {
         	a = a + b;
@@ -23,12 +24,7 @@ class GCD
         	a = a - b;
         }
         
-        double startTime = System.nanoTime();
-
         print(a,b,Euclidean_Algorithm(a,b));
-
-        double endTime = System.nanoTime();
-        System.out.println("\nTook "+((endTime - startTime)/Math.pow(10,9))+" seconds.");
 
         while(true)
         {
@@ -46,6 +42,7 @@ class GCD
     }
    static int Euclidean_Algorithm(int a, int b)
     {
+       // The idea is that for the equation a = bq + r, gcd(a, b) = gcd(b, r)
     	if (a%b == 0)
     		return b;
     	else
@@ -59,49 +56,4 @@ class GCD
         else
             System.out.println("("+a+","+b+") = " + rt);//(go check notebook)
     }
-
-//    int compute(int a, int b)
-//    {
-//    	//We need the absolute values of a and b since other classes which call this method can send negative integers too, and the algorithm works best with positive integers
-//        a = Math.abs(a);
-//        b = Math.abs(b);
-//        int q = 1, r = 1;
-//
-//        //my algorithm only works properly when a>b, so in case it's the other way around this condition will solve that problem by swapping the values
-//        if(b>a)
-//        {
-//            a = a + b;
-//            b = a - b;
-//            a = a - b;
-//        }
-//
-//        if(a == 0 || b == 0)
-//        {
-//            return 0;
-//        }
-//        else
-//        {
-//            //main while loop which gives us the GCD by implementing the Euclidean Algorithm
-//            while(r>0)//runs only for positive values for r.
-//            {
-//                q = 1;//need to restart from 1 since q is the multiplier
-//                
-//                //the loop below gives us the q which on multiplying with b gives us an integer closest to a
-//                while((q*b)<=a)
-//                {
-//                    q++;
-//                }
-//                q--;//need to reduce it by 1 since the while loop continues for an extra loop
-//                // ^^(try fixing this in the loop itself later)^^
-//                r = a-b*q;
-//                //System.out.println(a+" = "+b+" * "+q+" + "+r);
-//                if(r == 0)
-//                    break;
-//                //this here is where we get new a and b for the next equation for the division algorithm
-//                a = b;
-//                b = r;
-//            }
-//        }
-//        return b;
-//    }
 }
