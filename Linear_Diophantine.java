75870057217c7b6816f61385e2dae4d9163c2a59
Shayan1503 <91187603+Shayan1503@users.noreply.github.com>
2021-10-04 23:07:28 +0530

    Update Linear_Diophantine.java

--- Linear_Diophantine.java
+++ Linear_Diophantine.java
@@ -9,74 +9,76 @@ import java.util.*;
 class Linear_Diophantine
 {
 	static int a,b,c;
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        GCD gcd = new GCD();
-        Linear_Congruence cong = new Linear_Congruence();
-        
-        System.out.println("ax + by = c\nEnter the values for");
-        System.out.print("a = ");
-	a = sc.nextInt();
-	System.out.print("b = ");
-	b = sc.nextInt();
-	System.out.print("c = ");
-	c = sc.nextInt();
-		
-	int d = gcd.Euclidean_Algorithm(a, b);
-		
-	if(c%d == 0)
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args)
 	{
-		a /= d; 
-		b /= d; 
-		c /= d;
-
-		int x = cong.Congruence(a, c, b);
-		int y = (c-a*x)/b;
-		System.out.println("The given Linear Diophantine equation has infinite solutions in the form");
-		display(x,y);
-	}
-	else
-		System.out.println("The given Linear Diophantine equation has 0 solutions.");
+		driver();
 		
-	while(true)
-	{
-		System.out.println("Do you want to find the solution of more linear diophantine equations? \nPress 1 for 'Yes' or 0 'No'.");
-		int ch = sc.nextInt();
-	        switch(ch)
+		while(true)
 		{
-                    case 1: main(null);
-	            break;
-	            case 0: System.exit(0);
-	            break;
-	            default: System.out.println("Invalid input.");
-	        }
-	 }	
-    }
+			System.out.println("Do you want to find the solution of more linear diophantine equations? \nPress 1 for 'Yes' or 0 'No'.");
+			int ch = sc.nextInt();
+			
+			switch(ch)
+			{
+					case 1 -> driver();
+					case 0 -> System.exit(0);
+					default -> System.out.println("Invalid input.");
+			}
+		}
+	}
    
-    static void display(int X, int Y)
-    {
-    	int A = Math.abs(a);
-    	int B = Math.abs(b);
-    	if(a>0&&b>0)
-    	{
-    		System.out.println("x = " +X+ " + " +B+ "t");
-    		System.out.println("y = " +Y+ " - " +A+ "t");
-    	}
-    	else if(a>0&&b<0)
-    	{
-    		System.out.println("x = " +X+ " - " +B+ "t");
-    		System.out.println("y = " +Y+ " - " +A+ "t");
-    	}
-    	else if(a<0&&b>0)
-    	{
-    		System.out.println("x = " +X+ " + " +B+ "t");
-    		System.out.println("y = " +Y+ " + " +A+ "t");
-    	}
-    	else if(a<0&&b<0)
-    	{
-    		System.out.println("x = " +X+ " - " +B+ "t");
-    		System.out.println("y = " +Y+ " + " +A+ "t");
-    	}
-    }
+    	static void display(int X, int Y)
+   	{
+    		int A = Math.abs(a);
+    		int B = Math.abs(b);
+    		if(a>0&&b>0)
+    		{
+    			System.out.println("x = " +X+ " + " +B+ "t");
+    			System.out.println("y = " +Y+ " - " +A+ "t");
+    		}
+    		else if(a>0&&b<0)
+    		{
+    			System.out.println("x = " +X+ " - " +B+ "t");
+    			System.out.println("y = " +Y+ " - " +A+ "t");
+    		}
+    		else if(a<0&&b>0)
+    		{
+    			System.out.println("x = " +X+ " + " +B+ "t");
+    			System.out.println("y = " +Y+ " + " +A+ "t");
+    		}
+    		else if(a<0&&b<0)
+    		{
+    			System.out.println("x = " +X+ " - " +B+ "t");
+    			System.out.println("y = " +Y+ " + " +A+ "t");
+    		}
+   	}	
+	
+	static void driver()
+	{
+		System.out.println("ax + by = c\nEnter the values for");
+        	System.out.print("a = ");
+		a = sc.nextInt();
+		System.out.print("b = ");
+		b = sc.nextInt();
+		System.out.print("c = ");
+		c = sc.nextInt();
+		
+		int d = GCD.Euclidean_Algorithm(a, b);
+		
+		if(c%d == 0)
+		{
+			a /= d; 
+			b /= d; 
+			c /= d;
+
+			int x = Linear_Congruence.Congruence(a, c, b);
+			int y = (c-a*x)/b;
+			System.out.println("The given Linear Diophantine equation has infinite solutions in the form");
+			display(x,y);
+		}
+		else
+			System.out.println("The given Linear Diophantine equation has 0 solutions.");
+	}
 }
